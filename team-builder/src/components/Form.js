4148fe313eb2080d53d4cad8499ee1791c32f8e1
Nolan Picini <nolanpic@gmail.com>
2019-12-18 14:02:07 -0800

    Built out form

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Form = props => {
-    
-    
+
+    const { formValues, updateValue, submitForm } = props;
+
+    return (
+        <form>
+            <input
+                placeholder="Name"
+                type="text"
+                id="name"
+                value={formValues.name}
+                onChange={updateValue}
+            />
+            <input
+                placeholder="Email"
+                type="email"
+                id="email"
+                value={formValues.email}
+                onChange={updateValue}
+            />
+            <select id="role" onChange={updateValue}>
+                <option value="">-- Select a role --</option>
+            </select>
+
+            <button>Save</button>
+
+        </form>
+    );    
 };
 
 export default Form;
