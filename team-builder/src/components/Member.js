948f99314a6a95b185ca5662f7a1eb85bce9de69
Nolan Picini <nolanpic@gmail.com>
2019-12-18 15:03:01 -0800

    Cleaned up styling and removed duplicate roles

--- team-builder/src/components/Member.js
+++ team-builder/src/components/Member.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Member = props => {
-    const { member } = props;
+    const { member, editMember } = props;
     return (
         <tr>
             <td>{member.name}</td>
             <td>{member.email}</td>
             <td>{member.role}</td>
+            <td><button onClick={() => editMember(member)}>Edit</button></td>
         </tr>
     );
 };
